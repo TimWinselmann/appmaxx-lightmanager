8052e412e8b8bc098a5dc8713a36003974b5bdc8
TimWinselmann <twinselmann@googlemail.com>
2016-03-07 19:58:09 +0100

    Added templating mechanism via angularJS ui.router

--- frontend/js/lightmanager.js
+++ frontend/js/lightmanager.js
@@ -1,5 +1,35 @@
-var app = angular.module('lightmanager', []);
+var lightmanager = angular.module('lightmanager', ['ui.router']);
 
-app.controller('MainCtrl', function($scope) {
-	$scope.title = 'Lightmanager';
-});
+lightmanager.controller('MainCtrl',
+	function($scope) {
+		$scope.title = 'Lightmanager';
+	}
+);
+
+lightmanager.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+		$stateProvider
+	    .state('lights', {
+	    	url: '/lights',
+	    	templateUrl: 'partials/lights.html',
+	    	controller: 'LightsCtrl',
+	    	authenticate: false
+	    })
+	    .state('devices', {
+	    	url: '/devices',
+	    	templateUrl: 'partials/devices.html',
+	    	controller: 'DevicesCtrl',
+	    	authenticate: false
+	    });
+	  
+		$urlRouterProvider.otherwise('/lights');
+	}
+]);
+
+lightmanager.controller('LightsCtrl', ['$rootScope', '$scope', '$state', function($rootScope, $scope, $state) {
+	$scope.title  = 'Lights!';
+}]);
+
+lightmanager.controller('DevicesCtrl', ['$rootScope', '$scope', '$state', function($rootScope, $scope, $state) {
+	$scope.title  = 'Devices!';
+}]);
