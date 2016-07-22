3f7cbc58998472764a4353494a8162bb8757d2e1
TimWinselmann <twinselmann@googlemail.com>
2016-07-22 23:43:52 +0200

    improved error handling

--- WebContent/js/errorCtrl.js
+++ WebContent/js/errorCtrl.js
@@ -3,20 +3,26 @@ var lightmanager = angular.module('lightmanagerApp');
 lightmanager.controller("ErrorCtrl", [ "$scope", "$log", "$timeout", function($scope, $log, $timeout) {
 
 	$scope.$on('error.message', function(event, value) {
-
 		var errorMessage;
-
-		if (value && (value.message && value.description && value.statusCode)) {
-			errorMessage = value.description + ' (' + value.statusCode + ' ' + value.message + ')';
-		} else {
+		
+		/* build error message from json response */
+		if (value && value.data) {
+			var response = value.data;
+			if (response.message && response.description && response.statusCode) {
+				errorMessage = response.description + ' (' + response.statusCode + ' ' + response.message + ')';
+			}
+		}
+		
+		/* build general error message if we haven't one yet */
+		if (!errorMessage) {
 			errorMessage = 'An unexpected error occurred: ' + value.status + ' ' + value.statusText
-			    + '. Please see the browser console for more information.';
+		    	+ '. Please see the browser console for more information.';
 		}
 
 		$log.error('Error: ' + errorMessage);
-
 		$scope.errorMessage = errorMessage;
 
+		/* reset error message after 10 seconds */
 		$timeout(function() {
 			$scope.errorMessage = undefined;
 		}, 10000);
