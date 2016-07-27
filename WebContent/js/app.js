22f1eab0aa33948c2000e6fc7aec5d9e958e7aed
TimWinselmann <twinselmann@googlemail.com>
2016-07-27 22:41:12 +0200

    added ui.router and splitted index.html into different parts

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,1 +1,37 @@
-var lightmanager = angular.module('lightmanagerApp', []);
+var lightmanager = angular.module('lightmanagerApp', [ 'ui.router' ]);
+
+lightmanager.config([ "$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider.state('root', {
+		url : '',
+		abstract : true,
+		views : {
+			navigation : {
+				templateUrl : 'partials/navbar.html'
+			},
+			error : {
+				templateUrl : 'partials/error.html',
+				controller : 'ErrorCtrl'
+			},
+			footer : {
+				templateUrl : 'partials/footer.html',
+				controller : 'FooterCtrl'
+			}
+		}
+	});
+
+	$stateProvider.state('login', {
+		url : '/login',
+		views : {
+			'content@' : {
+				templateUrl : 'partials/login.html',
+				controller : 'LoginCtrl'
+			}
+		},
+		parent : 'root',
+		authenticate : false
+	});
+
+	$urlRouterProvider.otherwise('/login');
+
+} ]);
--- WebContent/js/headerCtrl.js
+++
@@ -1,9 +0,0 @@
-var lightmanager = angular.module('lightmanagerApp');
-
-lightmanager.controller("HeaderCtrl", [ "$scope", "$http", "AppmaxxService", function($scope, $http, AppmaxxService) {
-
-	$scope.$on('login.data', function(event, value) {
-		$scope.value = value;
-	});
-
-} ]);
