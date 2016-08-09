42cc71da92604cf2fbcf73521b9b2dc37d784813
Tim Winselmann <twinselmann@googlemail.com>
2016-08-09 16:52:34 +0200

    added todo: check login state on location change

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -43,8 +43,24 @@ lightmanager.config(["$stateProvider", "$urlRouterProvider", function($stateProv
 		parent : 'root',
 		authenticate : true
 	});
-	
-	
+
 	$urlRouterProvider.otherwise('/login');
 	
 } ]);
+
+// TODO TW check login state on location change and redirect to login page if not logged in
+//lightmanager.run(['$rootScope', '$location', '$cookieStore', '$http',
+//    function ($rootScope, $location, $cookieStore, $http) {
+//        // keep user logged in after page refresh
+//        $rootScope.globals = $cookieStore.get('globals') || {};
+//        if ($rootScope.globals.currentUser) {
+//            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+//        }
+//  
+//        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+//            // redirect to login page if not logged in
+//            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+//                $location.path('/login');
+//            }
+//        });
+    }]);
